0128a3d8a2d64090834598ce034b8450a535bffa
Emmanuel E <iodevblue@gmail.com>
2026-07-04 06:45:54 +0100

    add launch configuration and update index.js with object manipulation examples

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+
+const obj = {
+     name: "Something",
+     age: 29,
+     pos: "Cyber-Security"
+}
+console.log(obj)
+
+obj.cash = "$4,500"
+console.log('Obj\'s cash is ' + obj.cash)
+
+obj.cash = null
+console.log("Object's cash is nullified. Reprinting value...")
+console.log('Obj\'s cash is ' + obj.cash)
+
