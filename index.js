35f0eab3bf31cecbd3bd77f02ccdecd1b34759c4
Emmanuel E <iodevblue@gmail.com>
2026-07-29 07:56:08 +0100

    refactor main.js and index.js to streamline module imports and enhance code structure

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // import { main } from "#/index.js"
 
-const { main } = require("./src/iodevblue/sandbox/javascript/playground/index.js");
+const { main } = require("#/index.js");
 
-console.log("\n\nExecuting main...")
+console.log("\n\nExecuting main...");
 main();
 console.log("Terminating main...\n\n")
 
--- src/iodevblue/sandbox/javascript/playground/main.js
+++ src/iodevblue/sandbox/javascript/playground/main.js
@@ -1,21 +1,74 @@
+"use strict";
+
+// transform("Something", (item) => console.log(item))
+function transform(item, callback) {
+    callback(typeof item == "string" ? item.length : 0)
+}
+
+// doubleStep((item) => console.log(item)) ("This is for double step")
+function doubleStep(func) {
+    return (something) => {func(something)}
+}
+
+let prototype = {
+    name: "Primordial",
+    age: 0,
+    species: "Undefined",
+    isAlive() {
+        return this.age > 0;
+    },
+    speak(words) {
+        console.log(`${this.name} says: ${words}`);
+    }
+}
+
+class Student {
+    constructor(name, age, year) {
+        this.name = name
+        this.age = age
+        this.year = year
+    }
+
+    scream_exec() {
+        let stud = new Student("John Doe", 20, 2026)
+        Student.prototype.scream = function() {
+            console.log(`${this.name} is screaming ARRGGGGHHHH`)
+        }
+        stud.scream()
+
+        let symbol = Symbol("scream_louder")
+        Student.prototype[symbol] = function() {
+            console.log(`${this.name} is screaming louder AAAARRRRRRRGGGGGGGGHHHHHHHH`)
+        }
+        stud[symbol]()
+
+        console.log(symbol.description)
+    }
+
+    toJson() {
+        return JSON.stringify(this)
+    }
+}
+
+class UniversityStudent extends Student {
+    constructor(name, age, year, university) {
+        super(name, age, year)
+        this.university = university
+    }
+
+    verify_instance_of() {
+        let uniStudent = new UniversityStudent("Alice", 21, 2025, "MIT")
+        console.log(uniStudent instanceof Student)
+    }
+}
+
+// something = 79
+
 function main() {
-     const project = {
-          name: "Resilient Chain Protocol",
-          version: "1.0.0",
-          isActive: true
-     };
-
-     // 2 represents the number of spaces for indentation
-     const jsonString = JSON.stringify(project, null, 2);
-
-     console.log(jsonString);
-     /* Output:
-          {
-          "name": "Resilient Chain Protocol",
-          "version": "1.0.0",
-          "isActive": true
-          }
-     */
+    const vm = require('vm');
+    vm.runInNewContext('console.log("Hello")', { console: console });
 }
 
 module.exports = { main };
+
+    
